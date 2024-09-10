06a0aa7d69302ca36d7469394d6d68a8c81e7a73
HafsaAlkhaja <hafsaalkhaja@gmail.com>
2024-09-11 01:35:41 +0300

    added customisation popup

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-console.log("is it working?")
+console.log('is it working?')
 
 let button1 = document.querySelector('.button1')
 let button = document.querySelector('.instructions')
-button1.addEventListener('click', ()=>{
-   button.style.opacity = '1'
-    button.style.pointerEvents = 'all'
+let custom = document.querySelector('.button2')
+let gameC = document.querySelector('.custom1')
+let dark = document.querySelector('.btn-dark')
+let light = document.querySelector('.btn-light')
+let gameName = document.querySelector('.gameName')
+let closee = document.querySelector('.dialog__close')
+
+button1.addEventListener('click', () => {
+  button.style.opacity = '1'
+  button.style.pointerEvents = 'all'
+})
+
+custom.addEventListener('click', () => {
+  gameC.style.opacity = '1'
+  gameC.style.pointerEvents = 'all'
+})
+
+dark.addEventListener('click', () => {
+  gameName.style.backgroundColor = 'black'
+  gameName.style.color = 'white'
+  gameName.style.border = '10px solid white'
+})
+
+light.addEventListener('click', () => {
+  gameName.style.backgroundColor = 'white'
+  gameName.style.color = 'black'
+  gameName.style.border= '10px solid black'
+})
+
+closee.addEventListener('click', () => {
+  gameC.style.opacity = '0'
+  gameC.style.pointerEvents = 'none'
 })
