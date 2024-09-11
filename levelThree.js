4f2fef01a6da0b13705be24d8c1f0291b5463924
HafsaAlkhaja <hafsaalkhaja@gmail.com>
2024-09-11 12:53:57 +0300

    added more words

--- levelThree.js
+++ levelThree.js
@@ -4,7 +4,7 @@ let box = document.querySelector('.letter')
 const AnswerBar = document.querySelector('.newWord')
 const submit = document.querySelector('.subButton')
 let wordArray = []
-let words = ['colonel', 'epitome','nauseous', 'worcestershire', '']
+let words = ['colonel', 'epitome','nauseous', 'worcestershire', 'inundate', 'uncanny', 'acommodate', 'miniscule', 'fuchsia', 'zucchini', 'spaghetti', 'austin', 'asthma', 'paradigm', 'pneumonia', 'sapphire', 'aquamarine', 'aphrodite', '']
 const CopyWord = words.map((word) => word)
 const levelWinner = document.querySelector('.level-winner')
 const levelLoser = document.querySelector('.level-loser')
